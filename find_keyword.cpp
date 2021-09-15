effbdb4aa314b855ed9941b741ca0a4fa1e40206
gheao <1261773184@qq.com>
2021-09-15 19:31:07 +0800

    it can find key_num,switch_num and case_num

--- find_keyword.cpp
+++ find_keyword.cpp
@@ -1,30 +1,42 @@
 ﻿#pragma warning(disable:4996)
 #include<iostream>
 #include<stdio.h>
+#include<vector>
 #include<fstream>
 using namespace std;
 
-string Key_word[] =
+string Key_word[32] =
 {
-				   "auto","break","case","char","const","continue","default","do",
-				   "double","else","enum","extern","float","for","goto","if"
-				   "int","long","register","return","short","signed","sizeof","stastic"
-				   "struct","switch","typedef","union","unsigned","void","volatile","while"
+					  "auto","break","case","char","const","continue","default","do",
+					  "double","else","enum","extern","float","for","goto","if","int",
+					  "long","register","return","short","signed","sizeof","stastic",
+					  "struct","switch","typedef","union","unsigned","void","volatile","while"
 };
 
 string Text;
 int Total_num = 0;
 int Switch_num = 0;
-int Case_num = 0;
+int Single_case_num = 0;
+vector<int> Case_num;
 int If_else_num = 0;
 int If_else_if_else_num = 0;
 
 void Search_and_Outfile(string s)
 {
-	for (int i = 0; i <= 32; i++)
+	for (int i = 0; i <= 31; i++)
 	{
 		if (s == Key_word[i])
 		{
+			if (s == "switch")
+			{
+				Case_num.push_back(Single_case_num);
+				Switch_num++;
+				Single_case_num = 0;
+			}
+			if (s == "case")
+			{
+				Single_case_num++;
+			}
 			Total_num++;
 			break;
 		}
@@ -33,6 +45,19 @@ void Search_and_Outfile(string s)
 }
 void String_trim()
 {
+	int n = Text.size();
+	string change;
+	for (int i = 0; i < n; i++) {
+		if (Text[i] <= 'z' && Text[i] >= 'A')
+		{
+			change = change + Text[i];
+		}
+		else
+		{
+			break;
+		}
+	}
+	Text = change;
 	return;
 }
 
@@ -40,17 +65,33 @@ int main()
 {
 	int Find_type;
 	string File_p;
-	cin >> File_p >> Find_type;
+	cin >> File_p;
+	cin >> Find_type;
 	ifstream finput(File_p);
-	while(finput >> Text)
+
+	while (finput >> Text)
 	{
 		String_trim();
-		cout << Text << endl;
 		Search_and_Outfile(Text);
+
+	}
+	cout << "Total_num = " << Total_num << endl;
+	if (Find_type >= 2)
+	{
+		cout << "Switch_num = " << Switch_num << endl;
+		Case_num.push_back(Single_case_num);
+		cout << "Case_num = ";
+		for (int i = 1; i <= Switch_num; i++)
+		{
+			cout << Case_num[i] << " ";
+
+		}
+		cout << endl;
+	}
+	if (Find_type >= 3)
+	{
+		cout << "if_else_num = " << If_else_num << endl;
 	}
 	finput.close();
-	
 	return 0;
 }
-
-
